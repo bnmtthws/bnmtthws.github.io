186f08dae411f4dc744b673e18686d5574276556
ben matthews <ben.matthews@zoology.ubc.ca>
2020-11-09 13:31:29 -0800

    fixed .ignore

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,11 @@
+var store = [{
+        "title": "Welcome to the (Ben) Matthews Lab!",
+        "excerpt":"Welcome to the new home of the (Ben) Matthews lab in the Department of Zoology at the University of British Columbia. The physical doors will open in September, but we are here now. Of note: there are two Matthews labs in the department, and, quite confusingly, we both work on...","categories": ["lab","news"],
+        "tags": [],
+        "url": "http://localhost:4000/lab/news/welcome/",
+        "teaser":"http://localhost:4000/assets/mosquito.jpg"},{
+        "title": "Lab twitter",
+        "excerpt":"We have joined the twenty-first century. Come follow the lab on twitter @BenMatthewsUBC!   ","categories": ["lab","news"],
+        "tags": [],
+        "url": "http://localhost:4000/lab/news/twitter/",
+        "teaser":"http://localhost:4000/assets/mosquito.jpg"}]
